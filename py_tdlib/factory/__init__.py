fe30b5530a4ba33dce0f5df908f2015865566ae0
andrew <andrew@localhost>
2019-03-06 15:09:57 +0100

    patch table only if needed

--- py_tdlib/factory/__init__.py
+++ py_tdlib/factory/__init__.py
@@ -1,8 +1,12 @@
 from .utils import Type, Method, deserialize
-from .. import constructors
-
-# monkey patching constructors table
 from .table import constructors as cs
 
-for k in cs.keys():
-    cs[k] = getattr(constructors, k)
+# monkey patching constructors table
+
+if "__patched" not in cs:
+    from .. import constructors
+
+    for k in cs.keys():
+        cs[k] = getattr(constructors, k)
+
+    cs["__patched"] = True
